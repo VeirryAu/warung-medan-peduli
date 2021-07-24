150dff93685beca419fa22fe635abe670a54e438
VeirryAu <veirryau@gmail.com>
2021-07-24 13:12:04 +0700

    fix geolocation

--- warung.php
+++ warung.php
@@ -148,15 +148,11 @@ a, a:hover, a:focus, a:active {
      name: 'geolocation'
     }).then(function(result) {
         if (result.state == 'granted') {
-            report(result.state);
             geoBtn.style.display = 'none';
         } else if (result.state == 'prompt') {
-            report(result.state);
             geoBtn.style.display = 'none';
-
             navigator.geolocation.getCurrentPosition(revealPosition, positionDenied, geoSettings);
         } else if (result.state == 'denied') {
-            report(result.state);
             geoBtn.style.display = 'inline';
         }
         result.onchange = function() {
